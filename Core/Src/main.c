a3748e8c12d5296dfb40ad329fded51e6c7d4e52
Denis von Arx <dvarx@ethz.ch>
2025-06-17 13:43:54 +0200

    adjusted readme and python test script

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,11 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+This project contains the firmware code for the STM32LK432B for the MSRL electrostimulation driver.
+/*
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -31,12 +22,12 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+//if AUTOSTART is defined, the output will be automatically be enabled after startup
+#define AUTOSTART
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
@@ -64,31 +55,31 @@ static void MX_ADC1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-//UART related definitions
-uint32_t period=32000/2;
+// UART related definitions
+uint32_t period = 14285; // initial counter corresponds to f_pwm=1.12kHz
 uint8_t uart_rx_symbol;
 #define UART_BUFFER_SIZE 32
 uint8_t rx_buffer_counter;
 uint8_t rx_buffer[UART_BUFFER_SIZE];
 uint8_t tx_buffer[UART_BUFFER_SIZE];
-//commands & fsm
+// commands & fsm
 #define COMMAND_LENGTH 4
-const uint8_t CMD_START[]="STRT";
-const uint8_t CMD_STOP[]="STOP";
-const uint8_t CMD_CNTR[]="CNTR";
-//ADC related variables
+const uint8_t CMD_START[] = "STRT";
+const uint8_t CMD_STOP[] = "STOP";
+const uint8_t CMD_CNTR[] = "CNTR";
+// ADC related variables
 #define ADC_BUFFER_SIZE 1024
-uint32_t adc_val=0;
-uint16_t adc_buffer_counter=0;
+uint32_t adc_val = 0;
+uint16_t adc_buffer_counter = 0;
 uint32_t adc_buffer[ADC_BUFFER_SIZE];
-//pwm frequency related variables
-uint32_t des_freq_mHz=0;
+// pwm frequency related variables
+uint32_t des_freq_mHz = 0;
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
 
@@ -119,59 +110,67 @@ int main(void)
   MX_TIM1_Init();
   MX_ADC1_Init();
   /* USER CODE BEGIN 2 */
-  //setup pwm on Timer1 channel 1 and channel 2
-  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, period/2);
-  HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_1);
-  HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_1);
-  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, period/2);
-  HAL_TIM_PWM_Start(&htim1,TIM_CHANNEL_2);
-  HAL_TIMEx_PWMN_Start(&htim1,TIM_CHANNEL_2);
-  //light up LED3
+  // setup pwm on Timer1 channel 1 and channel 2
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, period / 2);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
+  __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, period / 2);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
+  htim1.Init.Period = period;
+  HAL_TIM_PWM_Init(&htim1);
+  // light up LED3
   HAL_GPIO_WritePin(LED_BLUE_GPIO_Port, LED_BLUE_Pin, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(SHUTDOWN_GPIO_Port, SHUTDOWN_Pin, GPIO_PIN_RESET);
-  //setup interrupt for Timer2
+  // start ADC
   HAL_ADC_Start_IT(&hadc1);
+  // start timer 2
   HAL_TIM_Base_Start_IT(&htim2);
+
+#ifdef AUTOSTART
+  // enable half-bridge drive
+  HAL_GPIO_WritePin(SHUTDOWN_GPIO_Port, SHUTDOWN_Pin, GPIO_PIN_RESET);
+#endif
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1){
+  while (1)
+  {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-	  //read a single character in interrupt mode
-	  HAL_UART_Receive_IT(&huart2,&uart_rx_symbol,1);
-	  //HAL_UART_Transmit(&huart2,uart_tx_buffer,5,100);
+    // read a single character in interrupt mode
+    HAL_UART_Receive_IT(&huart2, &uart_rx_symbol, 1);
+    // HAL_UART_Transmit(&huart2,uart_tx_buffer,5,100);
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Configure the main internal regulator output voltage
-  */
+   */
   if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
   {
     Error_Handler();
   }
 
   /** Configure LSE Drive Capability
-  */
+   */
   HAL_PWR_EnableBkUpAccess();
   __HAL_RCC_LSEDRIVE_CONFIG(RCC_LSEDRIVE_LOW);
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE|RCC_OSCILLATORTYPE_MSI;
+   * in the RCC_OscInitTypeDef structure.
+   */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE | RCC_OSCILLATORTYPE_MSI;
   RCC_OscInitStruct.LSEState = RCC_LSE_ON;
   RCC_OscInitStruct.MSIState = RCC_MSI_ON;
   RCC_OscInitStruct.MSICalibrationValue = 0;
@@ -183,9 +182,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -197,15 +195,15 @@ void SystemClock_Config(void)
   }
 
   /** Enable MSI Auto calibration
-  */
+   */
   HAL_RCCEx_EnableMSIPLLMode();
 }
 
 /**
-  * @brief ADC1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief ADC1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_ADC1_Init(void)
 {
 
@@ -221,7 +219,7 @@ static void MX_ADC1_Init(void)
   /* USER CODE END ADC1_Init 1 */
 
   /** Common config
-  */
+   */
   hadc1.Instance = ADC1;
   hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
@@ -243,7 +241,7 @@ static void MX_ADC1_Init(void)
   }
 
   /** Configure the ADC multi-mode
-  */
+   */
   multimode.Mode = ADC_MODE_INDEPENDENT;
   if (HAL_ADCEx_MultiModeConfigChannel(&hadc1, &multimode) != HAL_OK)
   {
@@ -251,7 +249,7 @@ static void MX_ADC1_Init(void)
   }
 
   /** Configure Regular Channel
-  */
+   */
   sConfig.Channel = ADC_CHANNEL_8;
   sConfig.Rank = ADC_REGULAR_RANK_1;
   sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
@@ -265,14 +263,13 @@ static void MX_ADC1_Init(void)
   /* USER CODE BEGIN ADC1_Init 2 */
 
   /* USER CODE END ADC1_Init 2 */
-
 }
 
 /**
-  * @brief TIM1 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM1 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM1_Init(void)
 {
 
@@ -290,7 +287,7 @@ static void MX_TIM1_Init(void)
   htim1.Instance = TIM1;
   htim1.Init.Prescaler = 1;
   htim1.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim1.Init.Period = 32000/2;
+  htim1.Init.Period = 32000 / 2;
   htim1.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim1.Init.RepetitionCounter = 0;
   htim1.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
@@ -340,14 +337,13 @@ static void MX_TIM1_Init(void)
 
   /* USER CODE END TIM1_Init 2 */
   HAL_TIM_MspPostInit(&htim1);
-
 }
 
 /**
-  * @brief TIM2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief TIM2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_TIM2_Init(void)
 {
 
@@ -362,7 +358,7 @@ static void MX_TIM2_Init(void)
 
   /* USER CODE END TIM2_Init 1 */
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 320-1;
+  htim2.Init.Prescaler = 320 - 1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 100;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
@@ -385,14 +381,13 @@ static void MX_TIM2_Init(void)
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
-
 }
 
 /**
-  * @brief USART2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief USART2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_USART2_UART_Init(void)
 {
 
@@ -420,14 +415,13 @@ static void MX_USART2_UART_Init(void)
   /* USER CODE BEGIN USART2_Init 2 */
 
   /* USER CODE END USART2_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
@@ -441,10 +435,10 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, SHUTDOWN_Pin|LED_BLUE_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, SHUTDOWN_Pin | LED_BLUE_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : SHUTDOWN_Pin LED_BLUE_Pin */
-  GPIO_InitStruct.Pin = SHUTDOWN_Pin|LED_BLUE_Pin;
+  GPIO_InitStruct.Pin = SHUTDOWN_Pin | LED_BLUE_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
@@ -457,66 +451,77 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE BEGIN 4 */
 
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-	//HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
-	HAL_ADC_Start_IT(&hadc1);
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  // HAL_GPIO_TogglePin(LD3_GPIO_Port, LD3_Pin);
+  HAL_ADC_Start_IT(&hadc1);
 }
 
-void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
-	//a command ends with the '\n' character
-	if(uart_rx_symbol=='\n'){
-		rx_buffer[rx_buffer_counter]=0;
-		process_command();
-		return;
-	}
-	else{
-		//write the received symbol to the buffer
-		rx_buffer[rx_buffer_counter]=uart_rx_symbol;
-	}
-	if(rx_buffer_counter==UART_BUFFER_SIZE){
-		//circular buffer
-		rx_buffer_counter=0;
-	}
-	else{
-		rx_buffer_counter=rx_buffer_counter+1;
-	}
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+  // a command ends with the '\n' character
+  if (uart_rx_symbol == '\n')
+  {
+    rx_buffer[rx_buffer_counter] = 0;
+    process_command();
+    return;
+  }
+  else
+  {
+    // write the received symbol to the buffer
+    rx_buffer[rx_buffer_counter] = uart_rx_symbol;
+  }
+  if (rx_buffer_counter == UART_BUFFER_SIZE)
+  {
+    // circular buffer
+    rx_buffer_counter = 0;
+  }
+  else
+  {
+    rx_buffer_counter = rx_buffer_counter + 1;
+  }
 }
 
-void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc){
-	adc_buffer[adc_buffer_counter]=HAL_ADC_GetValue(&hadc1);
-	adc_buffer_counter=(adc_buffer_counter+1)%ADC_BUFFER_SIZE;
-	return;
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+{
+  adc_buffer[adc_buffer_counter] = HAL_ADC_GetValue(&hadc1);
+  adc_buffer_counter = (adc_buffer_counter + 1) % ADC_BUFFER_SIZE;
+  return;
 }
 
-void process_command(){
-	if(strcmp(CMD_START,rx_buffer)==0){
-		//execute START command
-		HAL_GPIO_TogglePin(LED_BLUE_GPIO_Port, LED_BLUE_Pin);
-		HAL_GPIO_WritePin(SHUTDOWN_GPIO_Port, SHUTDOWN_Pin, GPIO_PIN_RESET);
-	}
-	if(strcmp(CMD_STOP,rx_buffer)==0){
-		//execute STOP command
-		HAL_GPIO_TogglePin(LED_BLUE_GPIO_Port, LED_BLUE_Pin);
-		HAL_GPIO_WritePin(SHUTDOWN_GPIO_Port, SHUTDOWN_Pin, GPIO_PIN_SET);
-	}
-	if(strcmp(CMD_CNTR,rx_buffer)==0){
-		//set the desired frequency
-		uint32_t period=atoi(rx_buffer+COMMAND_LENGTH+1);
-		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, period/2);
-		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, period/2);
-		htim1.Init.Period = period;
-		HAL_TIM_PWM_Init(&htim1);
-	}
-	//reset the buffer counter
-	rx_buffer_counter=0;
+void process_command()
+{
+  if (strcmp(CMD_START, rx_buffer) == 0)
+  {
+    // execute START command
+    HAL_GPIO_TogglePin(LED_BLUE_GPIO_Port, LED_BLUE_Pin);
+    HAL_GPIO_WritePin(SHUTDOWN_GPIO_Port, SHUTDOWN_Pin, GPIO_PIN_RESET);
+  }
+  if (strcmp(CMD_STOP, rx_buffer) == 0)
+  {
+    // execute STOP command
+    HAL_GPIO_TogglePin(LED_BLUE_GPIO_Port, LED_BLUE_Pin);
+    HAL_GPIO_WritePin(SHUTDOWN_GPIO_Port, SHUTDOWN_Pin, GPIO_PIN_SET);
+  }
+  if (strcmp(CMD_CNTR, rx_buffer) == 0)
+  {
+    // set the desired frequency
+    uint32_t period = atoi(rx_buffer + COMMAND_LENGTH + 1);
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, period / 2);
+    __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, period / 2);
+    htim1.Init.Period = period;
+    HAL_TIM_PWM_Init(&htim1);
+  }
+  // reset the buffer counter
+  rx_buffer_counter = 0;
 }
 
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -528,14 +533,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
--- readme.md
+++ readme.md
@@ -1,14 +1,27 @@
-#Electrostimulation Project
+# Electrostimulation Project
 
-##Commands
-###Frequency Command
-The frequency command is used to set the frequency of the PWM signal. It has the following format:
+This project contains the Firmware for the STM32L432KB used in the electrostimulation driver PCB. A simple script which demonstrates turning the output on and off and setting of the PWM frequency is provided in `EMDriver.py`.
 
-`FREQ\0XXXXXXXX\n`
+<p align="center">
+  <img src="doc/em_driver_pic.png" width=400/>
+  <br>
+  <a href="https://github.com/dvarx/irs_driver">Electrostimulation Driver</a>
+</p>
 
-where `XXXXXXXX` describes the desired frequency in mHz.
+## Commands
 
-##Export data from STM32 CubeIDE Debugger
+|Command Syntax|Meaning|
+|----|----|
+|`STRT\n`|Starts PWM output|
+|`STOP\n`|Stops PWM output|
+|`CNTR\0XXXXXXXX\n`| Sets the counter limit of the internal PWM counter
+
+The resulting PWM frequency is given by $f_{PWM}=\frac{counter}{2^{24}}$.
+
+## Autostart
+To enable the output of the driver upon powerup, `#define AUTOSTART` in `main.c`.
+
+## Export data from STM32 CubeIDE Debugger
 - Display the memory browser `Windows`->`Show View`->`Memory Browser`
 - Export the data as shown below and use `plot_memory_buffer.py` to visualize the data
 ![Memory browser](doc/image.png)
